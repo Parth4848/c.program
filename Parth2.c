ba7d7002505266f7a98341b3528a750bfde02886
Parth Thumar <parth.shaswat@gmail.com>
2025-08-08 10:56:04 +0530

    Add files via upload

---
+++ Parth2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int num1,num2,sum;
+
+    printf("Frist number :");
+    scanf("%d",&num1);
+
+    printf("Second number :");
+    scanf("%d",&num2);
+
+    sum=num1+num2;
+
+    printf("This is my sum : %d",sum );
+    return 0;
+
+
+}
