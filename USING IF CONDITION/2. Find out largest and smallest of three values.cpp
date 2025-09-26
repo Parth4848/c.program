cb80a12f3ab468028b9e0bebb106d167f04cba8b
Parth Thumar <parth.shaswat@gmail.com>
2025-09-26 10:37:43 +0530

    Add files via upload

---
+++ USING IF CONDITION/2. Find out largest and smallest of three values.cpp
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+int main(){
+    int x,y,z;
+    printf("Enter value of X=");
+    scanf("%d",&x);
+    printf("Enter value of Y=");
+    scanf("%d",&y);
+    printf("Enter value of Z=");
+    scanf("%d",&z);
+    if (x>=y && x>=z)
+    {
+        printf("X is largest\n");
+        if (y>=z)
+        {
+            printf("z is smallest\n");
+        }
+        else
+        {
+            printf("y is smallest\n");
+        }
+
+    }
+    else if (y>=x && y>=z)
+    {
+        printf("Y is largest\n");
+        if (x>=z)
+        {
+            printf("z is smallest\n");
+        }
+        else
+        {
+            printf("x is smallest\n");
+        }
+
+    }
+    else
+    {
+        printf("z is greatest\n");
+        if (x>=y)
+        {
+            printf("y is the smallest\n");
+        } else
+        {
+            printf("x is the smallest\n");
+        }
+
+
+    }
+
+    return 0;
+}
