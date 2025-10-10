90ba9a6b63cf445d52af352851e8d1d34ef33c5c
Parth Thumar <parth.shaswat@gmail.com>
2025-10-10 10:31:42 +0530

    Add files via upload

---
+++ USING LOOPS/8. Print sum of 1st n odd nos.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int n,sum=0;
+    printf("Enter N=");
+    scanf("%d",&n);
+
+    for (int i = 0; i <=2*n-1 ; i++)
+    {
+        if (i%2!=0)
+        {
+            sum+=i;
+        }
+
+    }
+    printf("%d",sum);
+    return 0;
+}
